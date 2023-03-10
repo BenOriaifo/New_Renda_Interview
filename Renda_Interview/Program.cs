e30f0294893bf5718e7f1aa941877d9c6d65092e
Benjamin Oriaifo <bennyova@yahoo.com>
2023-03-10 18:00:50 +0100

    fix for duplicate array result

---
+++ Renda_Interview/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renda_Interview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var values = new int[] { 1, 2, 2, 3, 3, 4, 5, 5 };
+            Console.WriteLine(RemoveDuplicateFromArray(values));
+            Console.ReadLine();
+        }
+
+        static int[] RemoveDuplicateFromArray(int[] duplicateArray)
+        {
+            //var duplicateArray = new int[]{ 1, 2, 2, 3, 3, 4, 5, 5};
+            var responseHashSet = new HashSet<int>();
+            if(duplicateArray != null && duplicateArray.Any())
+            {
+                for(int i = 0; i < duplicateArray.Length; i++)
+                {
+                    var item = duplicateArray[i];
+                    responseHashSet.Add(item);
+                    Console.WriteLine(item);
+                }
+            }
+            return responseHashSet.ToArray();
+        }
+    }
+}
